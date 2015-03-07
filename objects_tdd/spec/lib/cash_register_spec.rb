917d7e9e651cc46df488dcbd530e8e3fa1d4518c
Bill LeBorgne <bill@leborgne.name>
2015-03-07 16:31:28 -0700

    [#89855526] Add a pay test and refactor
    Minor refactoring of test code
    Add a test case for underpayment

--- objects_tdd/spec/lib/cash_register_spec.rb
+++ objects_tdd/spec/lib/cash_register_spec.rb
@@ -12,7 +12,7 @@ describe CashRegister do
     end
   end
 
-  context 'has a #purchase method' do
+  describe 'has a #purchase method' do
     it 'takes a floating point value and adds it to the total' do
       expect(subject.total).to eq(0.0)
       purchase_amount = 3.14
@@ -21,16 +21,42 @@ describe CashRegister do
     end
   end
 
-  context 'has a #pay method' do
-    it 'takes a floating point value and returns the amount of change' do
-      expect(subject.total).to eq(0.0)
-      purchase_amount = 3.14
-      payment_amount = 5.00
-      expected_change = payment_amount - purchase_amount
-      expected_results = "Your change is $#{format('%.2f', expected_change)}"
-      subject.purchase(purchase_amount)
-      expect(subject.pay(payment_amount)).to eq(expected_results)
-      expect(subject.total).to eq(0.0)
+  describe 'has a #pay method that takes a floating point value' do
+    context 'for amounts greater than the current total' do
+      it 'returns a string indicating the amount of change' do
+        expect(subject.total).to eq(0.0)
+        purchase_amount = 3.14
+        payment_amount = 5.00
+        expected_change = payment_amount - purchase_amount
+        expected_string = "Your change is $#{format('%.2f', expected_change)}"
+        subject.purchase(purchase_amount)
+        expect(subject.pay(payment_amount)).to eq(expected_string)
+        expect(subject.total).to eq(0.0)
+      end
+    end
+
+    context 'for amounts less than the current total' do
+      it 'returns a string indicating the updated total' do
+        expect(subject.total).to eq(0.0)
+        purchase_amount = 2.50
+        payment_amount = 1.00
+        new_total = purchase_amount - payment_amount
+        expected_string = "Your new total is $#{format('%.2f', new_total)}"
+        subject.purchase(purchase_amount)
+        expect(subject.pay(payment_amount)).to eq(expected_string)
+        expect(subject.total).to eq(new_total)
+      end
+    end
+
+    context 'for amounts equal to the current total' do
+      it 'returns a string indicating no change is due' do
+        expect(subject.total).to eq(0.0)
+        amount = 2.68
+        expected_string = 'Your change is $0.00'
+        subject.purchase(amount)
+        expect(subject.pay(amount)).to eq(expected_string)
+        expect(subject.total).to eq(0.0)
+      end
     end
   end
 
@@ -39,13 +65,13 @@ describe CashRegister do
       purchase_amount = 3.78
       payment_amount = 5.00
       expected_change = payment_amount - purchase_amount
-      expected_result = "Your change is $#{format('%.2f', expected_change)}"
+      expected_string = "Your change is $#{format('%.2f', expected_change)}"
       expect(subject.total).to eq(0.0)
       subject.purchase(purchase_amount)
-      expect(subject.pay(payment_amount)).to eq(expected_result)
+      expect(subject.pay(payment_amount)).to eq(expected_string)
     end
 
-    it 'correctly handles underpayment as well as overpayment' do
+    it 'correctly handles multiple purchases and payments' do
       purchase_1 = 3.78
       purchase_2 = 5.22
       total = 9.00
