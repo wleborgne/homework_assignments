452ad11e638ab82efb185f85d47020163697222e
Bill LeBorgne <bill@leborgne.name>
2015-03-08 16:33:33 -0600

    [#89877554] Add initial attribute tests
    Add initialize method and tests
    The context block around these is bothering me.  Might remove
    the first 'is a Tablet' test later.

--- objects_tdd/lib/tablet.rb
+++ objects_tdd/lib/tablet.rb
@@ -1,3 +1,10 @@
 # Implements basic Tablet functionality
 class Tablet
+  attr_reader :brand, :os, :os_version
+
+  def initialize(brand, os, version)
+    @brand = brand
+    @os = os
+    @os_version = version
+  end
 end
--- objects_tdd/spec/lib/tablet_spec.rb
+++ objects_tdd/spec/lib/tablet_spec.rb
@@ -2,7 +2,26 @@ require 'rspec'
 require_relative '../../lib/tablet'
 
 describe Tablet do
-  it 'should be a Tablet' do
-    expect(subject).to be_a(Tablet)
+  let(:brand) { 'Apple' }
+  let(:os) { 'iOS' }
+  let(:version) { 7.1 }
+  let(:subject) { Tablet.new(brand, os, version) }
+
+  context 'The tablet' do
+    it 'should be a Tablet' do
+      expect(subject).to be_a(Tablet)
+    end
+
+    it 'has a #brand method to report the brand' do
+      expect(subject.brand).to eq(brand)
+    end
+
+    it 'has a #os method to report the operating system' do
+      expect(subject.os).to eq(os)
+    end
+
+    it 'has a #os_version method to report the operating system version' do
+      expect(subject.os_version).to eq(version)
+    end
   end
 end
