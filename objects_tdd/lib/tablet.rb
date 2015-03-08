d1b25004669bed8440e30b2c8bb7999889114df3
Bill LeBorgne <bill@leborgne.name>
2015-03-08 17:18:05 -0600

    [#89877554] Add app management methods and tests

--- objects_tdd/lib/tablet.rb
+++ objects_tdd/lib/tablet.rb
@@ -2,9 +2,38 @@
 class Tablet
   attr_reader :brand, :os, :os_version
 
+  DEFAULT_APPS = { 'Phone' => false, 'Messages' => false,
+                   'Contacts' => false, 'Calendar' => false }
+
+  # Initialize state with supplied parameters,
+  # and apps as the default apps
   def initialize(brand, os, version)
     @brand = brand
     @os = os
     @os_version = version
+    reset_apps
+  end
+
+  # App management methods
+
+  # Currently installed apps
+  def apps
+    @apps.keys
+  end
+
+  # Install a new app
+  def install_app(app_name)
+    @apps[app_name] = true
+  end
+
+  # Uninstall app if is not a default app
+  def uninstall_app(app_name)
+    @apps.delete(app_name) if @apps[app_name]
+  end
+
+  # Reset apps to default
+  def reset_apps
+    @apps = Hash.new(false)
+    @apps.merge!(DEFAULT_APPS)
   end
 end
--- objects_tdd/spec/lib/tablet_spec.rb
+++ objects_tdd/spec/lib/tablet_spec.rb
@@ -24,4 +24,39 @@ describe Tablet do
       expect(subject.os_version).to eq(version)
     end
   end
+
+  context 'can report and manage apps' do
+    let(:default_apps) { %w(Phone Messages Contacts Calendar) }
+
+    it 'has 4 default #apps, Phone, Messages, Contacts, Calendar' do
+      apps = subject.apps
+      expect(apps.length).to be(4)
+      expect(apps).to contain_exactly(*default_apps)
+    end
+
+    it 'can install apps via #install_app(name)' do
+      expected_apps = default_apps.push('New App')
+      subject.install_app('New App')
+      expect(subject.apps).to contain_exactly(*expected_apps)
+    end
+
+    it '#uninstall_app does not succeed for default apps' do
+      expect(subject.uninstall_app('Calendar')).to be_falsey
+    end
+
+    it '#uninstall_app succeeds for non-default apps' do
+      subject.install_app('New App')
+      expect(subject.apps.length).to eq(5)
+      expect(subject.uninstall_app('New App')).to be_truthy
+      expect(subject.apps).to contain_exactly(*default_apps)
+    end
+
+    it '#reset_apps will reset installed apps to the default' do
+      subject.install_app('new app 1')
+      subject.install_app('new app 2')
+      expect(subject.apps.length).to eq(6)
+      subject.reset_apps
+      expect(subject.apps).to contain_exactly(*default_apps)
+    end
+  end
 end
