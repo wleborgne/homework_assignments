7e84219a84aae7438c5cb72aef580732031684ae
Bill LeBorgne <bill@leborgne.name>
2015-03-10 15:23:14 -0600

    [#88972982] Add example code to file
    Add examples to class definition file
    Refactor to have purchase and total return formatted
    strings rather than raw numerical values

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -3,16 +3,20 @@
 # class CashRegister
 # Implements a simple cash register
 class CashRegister
-  attr_reader :total
 
   # Initial total is zero
   def initialize
     @total = 0.0
   end
 
+  # Return total as a formatted string
+  def total
+    format_money(@total)
+  end
   # Add purchase amount to total
   def purchase(purchase_amount)
     @total += purchase_amount
+    format_money(@total)
   end
 
   # Receive payment and calculate new total.
@@ -22,7 +26,7 @@ class CashRegister
   def pay(payment)
     if payment >= @total
       change = payment - @total
-      @total = 0
+      @total = 0.0
       "Your change is #{format_money(change)}"
     else
       @total -= payment
@@ -36,4 +40,31 @@ class CashRegister
   def format_money(number)
     "$#{format('%.2f', number)}"
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+
+  # Scenario 1
+  # Single purchase, single payment > total
+  puts 'Example scenario #1'
+  register = CashRegister.new
+  puts "Total = #{register.total}"
+  puts "Purchase = #{register.purchase(3.78)}"
+  puts "Total = #{register.total}"
+  puts "After payment: #{register.pay(5.00)}"
+  puts "Total = #{register.total}"
+
+  # Scenario 2
+  # Multiple purchases, multiple payments
+  # The new instance is probably unnecessary
+  puts 'Example Scenario #2'
+  register = CashRegister.new
+  puts "Total = #{register.total}"
+  puts "Purchase = #{register.purchase(3.78)}"
+  puts "Purchase = #{register.purchase(5.22)}"
+  puts "Total = #{register.total}"
+  puts "After payment: #{register.pay(5.00)}"
+  puts "Total = #{register.total}"
+  puts "After payment: #{register.pay(5.00)}"
+  puts "Total = #{register.total}"
 end
